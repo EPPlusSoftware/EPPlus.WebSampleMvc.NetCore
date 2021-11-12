6cf1a0ab956bbdde2ef8c61ab9f781eb2663f81b
swmal <{ID}+username}@users.noreply.github.com>
2021-11-12 11:48:52 +0100

    Added table export sample
    added draft of html-export sample

---
+++ Controllers/HtmlExportController.cs
@@ -0,0 +1,30 @@
+﻿using EPPlus.WebSampleMvc.NetCore.Models.HtmlExport;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPPlus.WebSampleMvc.NetCore.Controllers
+{
+    public class HtmlExportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExportTable1(string style)
+        {
+            if(!Enum.TryParse<TableStyles>(style, out TableStyles ts))
+            {
+                ts = TableStyles.Dark1;
+            }
+            ViewData["TableStyle"] = ts.ToString();
+            var model = new ExportTable1Model();
+            model.SetupSampleData(ts);
+            return View(model);
+        }
+    }
+}
---
+++ Models/HtmlExport/ExportTable1Model.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Faker;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
+
+namespace EPPlus.WebSampleMvc.NetCore.Models.HtmlExport
+{
+    public class ExportTable1Model
+    {
+        public ExportTable1Model()
+        {
+
+        }
+
+        private DataTable _dataTable;
+
+        private void InitDataTable()
+        {
+            _dataTable = new DataTable();
+            _dataTable.Columns.Add("FirstName", typeof(string));
+            _dataTable.Columns.Add("LastName", typeof(string));
+            _dataTable.Columns.Add("BirthDate", typeof(DateTime));
+            _dataTable.Columns.Add("City", typeof(string));
+
+            for(var x = 0; x < 50; x++)
+            {
+                _dataTable.Rows.Add(Faker.Name.First(), Faker.Name.Last(), Faker.Identification.DateOfBirth(), Faker.Address.City());
+            }
+
+        }
+
+        public void SetupSampleData(TableStyles style = TableStyles.Dark1)
+        {
+            InitDataTable();
+            using(var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("Html export sample 1");
+                var tableRange = sheet.Cells["A1"].LoadFromDataTable(_dataTable, true, style);
+                var table = sheet.Tables.GetFromRange(tableRange);
+                Css = table.HtmlExporter.GetCssString();
+                Html = table.HtmlExporter.GetHtmlString();
+            }
+        }
+
+        public string Css { get; set; }
+
+        public string Html { get; set; }
+
+
+    }
+}
